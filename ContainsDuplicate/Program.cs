176bc582b3a4015ff89e31b35970fd5f4d107dcb
Shachar <shacharmiz4@gmail.com>
2025-07-17 22:17:19 +0300

    Initial commit with LeetCode and Schooldir structure

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4 };
+        bool result = ContainsDuplicate(nums);
+        Console.WriteLine(result); // Output: True
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> seenNumbers = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (seenNumbers.Contains(num))
+            {
+                return true;
+            }
+            seenNumbers.Add(num);
+        }
+        return false;
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> ints = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (ints.ContainsKey(complement))
+            {
+                Console.WriteLine($"Indices: [{ints[complement]}, {i}]");
+                return new int[] { ints[complement], i };
+            }
+
+            if (!ints.ContainsKey(nums[i]))
+            {
+                ints[nums[i]] = i;
+            }
+        }
+        return new int[] { };
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+       
+        
+        TwoSum(nums,target) ; // Output: Indices: [0, 1]
+    }
+}
